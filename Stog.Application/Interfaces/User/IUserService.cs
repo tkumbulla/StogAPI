d805393548196b0b6bb0a322ce0636dfd2b92de0
Edra Tabaku <edra.tabaku@pragmatic.al>
2022-10-22 21:00:07 +0200

    Added user service that will handle all operations regarding user management.

---
+++ Stog.Application/Interfaces/User/IUserService.cs
@@ -0,0 +1,89 @@
+﻿using Stog.Application.Models.User;
+using Stog.Application.Results;
+using Stog.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stog.Application.Interfaces.User
+{
+    /// <summary>
+    /// Defines the structure of the IUserService interface.
+    /// This interface defines all the operations regarding user management.
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Get all users according to filters
+        /// </summary>
+        /// <returns></returns>
+        Task<HashSet<UserModel>> GetUsers();
+
+        /// <summary>
+        /// Get all roles.
+        /// </summary>
+        /// <returns>.</returns>
+        IQueryable<RoleModel> GetRoles();
+
+        /// <summary>
+        /// Get user role.
+        /// </summary>
+        /// <param name="user">.</param>
+        /// <returns>.</returns>
+        Task<string> GetUserRoleAsync(ApplicationUser user);
+
+        /// <summary>
+        /// Get logged in user details.
+        /// </summary>
+        /// <returns>.</returns>
+        Task<UserModel> GetCurrentUserDetailsAsync();
+
+        /// <summary>
+        /// Get user details by id.
+        /// </summary>
+        /// <param name="id">.</param>
+        /// <returns>User View Model.</returns>
+        UserModel GetUserById(Guid id);
+        /// <summary>
+        /// Get the details of a user from its identifier
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        Task<UserModel> GetUserByIdAsync(Guid id);
+
+        /// <summary>
+        /// Get current user id.
+        /// </summary>
+        /// <returns>.</returns>
+        Guid GetCurrentUserId();
+
+        /// <summary>
+        /// Update an existing user.
+        /// </summary>
+        /// <param name="user">.</param>
+        /// <returns>.</returns>
+        Task<Result> UpdateUser(UserModel user);
+
+        /// <summary>
+        /// Delete an existing user.
+        /// </summary>
+        /// <param name="user">.</param>
+        /// <returns>Fail if the user cannot be found and Ok if the method is completed successfully.</returns>
+        Task<Result> DeleteUser(UserModel user);
+        /// <summary>
+        /// Recover a deleted user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Result> RecoverUser(Guid id);
+        /// <summary>
+        /// Change user password
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task<Result> ChangePassword(ChangePasswordModel model);
+    }
+}
---
+++ Stog.Application/Models/User/UserModel.cs
@@ -0,0 +1,131 @@
+﻿using Stog.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stog.Application.Models.User
+{
+    /// <summary>
+    /// Model needed to display the needed information for users
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// Parameterless constructor
+        /// </summary>
+        public UserModel() 
+        {
+            Id = new Guid();
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            UserName = string.Empty;
+            CreatedOnUtc = DateTime.UtcNow;
+            UpdatedOnUtc = DateTime.UtcNow;
+            IsActive = false;
+            Email = string.Empty;
+            Password = string.Empty;
+            Role = string.Empty;
+            CreatedBy = string.Empty;
+            UpdatedBy = string.Empty;
+            DisplayUserName = string.Empty;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="user"></param>
+        public UserModel(ApplicationUser user)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            UserName = user.UserName;
+            CreatedOnUtc = user.CreatedOnUtc;
+            UpdatedOnUtc = user.UpdatedOnUtc;
+            IsActive = user.IsActive;
+            Email = user.Email;
+            Password = user.PasswordHash;
+            Role = string.Empty;
+            CreatedBy = string.Empty;
+            UpdatedBy = string.Empty;
+            DisplayUserName = user.UserName;
+        }
+        /// <summary>
+        /// User id
+        /// </summary>
+        public Guid? Id { get; set; }
+        /// <summary>
+        ///  First name of the user
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        ///  Last name of the user
+        /// </summary>
+
+        public string LastName { get; set; }
+        /// <summary>
+        ///  Full name of the user
+        /// </summary>
+        public string FullName => FirstName + " " + LastName;
+        /// <summary>
+        /// User's username (used for logging in)
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// User's email adress
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UTC date time when the entity was first created.
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UTC date time when the entity was last updated.
+        /// </summary>
+        public DateTime? UpdatedOnUtc { get; set; }
+        /// <summary>
+        /// Shows if the entity is active or not
+        /// </summary>
+        public bool IsActive { get; set; }
+        /// <summary>
+        /// Shows the user's password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Unique identifier of the user's role
+        /// </summary>
+        public Guid? Roleid { get; set; }
+
+        /// <summary>
+        /// Name of the role
+        /// </summary>
+        public string Role { get; set; }
+        /// <summary>
+        /// The identifier of the user that created the user.
+        /// </summary>
+        public Guid CreatedById { get; set; }
+        /// <summary>
+        /// The full name of the user that created the user.
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// The identifier of the user that last updated the user.
+        /// </summary>
+        public Guid? UpdatedById { get; set; }
+        /// <summary>
+        /// The full name of the user that last updated the user.
+        /// </summary>
+        public string UpdatedBy { get; set; }
+        /// <summary>
+        /// Display username
+        /// </summary>
+        public string DisplayUserName { get; set; }
+        /// <summary>
+        /// True if the record is deleted
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
