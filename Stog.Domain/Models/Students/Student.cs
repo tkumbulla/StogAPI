7b09b8a047c44e73dc574ef9373e2f8fccbcd1c5
tkumbulla <tajdakumbulla@outlook.com>
2022-10-22 19:16:57 +0200

    Added student entity

---
+++ Stog.Domain/Models/Students/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stog.Domain.Models.Students
+{
+    public class Student
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Student()
+        {
+            SSN = String.Empty;
+            StudentCard = new Common.File();
+        }
+        /// <summary>
+        /// The social security number of the student
+        /// </summary>
+        public string SSN { get; set; }
+        /// <summary>
+        /// The file that corresponds to the student id card
+        /// </summary>
+        public Guid StudentCardId { get; set; }
+        /// <summary>
+        /// The file that corresponds to the student id card
+        /// </summary>
+        public virtual Common.File StudentCard { get;set; }
+    }
+}
---
+++ Stog.Domain/Models/Students/StudentBankAccount.cs
@@ -0,0 +1,30 @@
+﻿using Stog.Domain.Models.Institutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stog.Domain.Models.Students
+{
+    public class StudentBankAccount
+    {
+        public StudentBankAccount()
+        {
+            IBAN = String.Empty;
+            FinancialInstitution = new FinancialInstitution();
+        }
+        /// <summary>
+        /// The iban of the student
+        /// </summary>
+        public string IBAN { get; set; }
+        /// <summary>
+        /// The institution which this account belongs to
+        /// </summary>
+        public Guid FinanacialInstitutionId { get; set; }
+        /// <summary>
+        /// The institution which this account belongs to
+        /// </summary>
+        public virtual FinancialInstitution FinancialInstitution { get; set; }
+    }
+}
